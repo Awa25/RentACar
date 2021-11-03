bedf827a4869ab8de2e0691c4c2fce4afa696355
Ruby <72461720+k773352@users.noreply.github.com>
2021-11-03 22:36:23 +0000

    edit code

--- src/ooc/yoursolution/Car.java
+++ src/ooc/yoursolution/Car.java
@@ -25,7 +25,7 @@ public class Car implements CarInterface {
       this.id=id;
       this.make = make;
       this.rate = rate;
-//      craeteAvailability();
+      craeteAvailability();
     }
     
     @Override
@@ -102,8 +102,8 @@ public class Car implements CarInterface {
       return false;
     }
 
-//    private void craeteAvailability() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
+    private void craeteAvailability() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
